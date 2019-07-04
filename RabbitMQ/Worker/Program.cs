59e1cb8d4b0601831c771d4507ca3aa3d6f4cc10
tj_akd <cherish1224@outlook.com>
2019-07-04 18:24:21 +0800

    work queue

---
+++ RabbitMQ/Worker/Program.cs
@@ -0,0 +1,61 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Worker Start!");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "task_queue",
+                                        durable: true,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                    Console.WriteLine(" [*] Waiting for message.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("[X] Received {0}", message);
+
+                        int dots = message.Split('.').Length - 1;
+                        Thread.Sleep(dots * 1000);
+
+                        Console.WriteLine("[X] Done.");
+
+                        // 通知消息已处理完
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    };
+
+                    channel.BasicConsume(queue: "task_queue",
+                                        autoAck: false,
+                                        consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+
+
+
+        }
+    }
+}
